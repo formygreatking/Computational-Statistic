eeb6bc96b6d7920e3f7c3ce558a327c86259ae2e
CRY <670110891@qq.com>
2018-04-11 19:33:18 +0800

    add em4gmm

--- experiment4.1/EM-bp.py
+++ experiment4.1/EM-bp.py
@@ -1,24 +1,75 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import copy
 import matplotlib.pyplot as plt
 import math
-from scipy import stats
 
-n = 20
-p = 0.5
-k = np.arange(0,50)  
-binomial = stats.binom.pmf(k,n,p)  
-rv = stats.poisson(20)
-y_rv = []
-for i in range(50):
-    y_rv.append(rv.pmf(i))
-print(binomial)
-print(y_rv)
-  
-  
-plt.plot(k, binomial, y_rv, 'o-')  
-plt.title('binomial:n=%i,p=%.2f'%(n,p),fontsize=15)  
-plt.xlabel('number of success')  
-plt.ylabel('probalility of success', fontsize=15)  
-plt.grid(True)  
-plt.show()  
+def gen_data(mu, sigma, N):
+    
+    X = np.zeros((N, 1))
+    X = np.matrix(X)  
+    from_1 = 0
+    from_2 = 0
+    for i in range(N):
+        if np.random.random() < 0.6:
+            X[i] = np.random.normal(mu[0], sigma[0], 1)
+            from_1 += 1
+        else:
+            X[i] = np.random.normal(mu[1], sigma[1], 1) 
+            from_2 += 1
+    print(from_1, from_2)
+    alpha = [0.5, 0.5]
+    mu = np.random.random((2,1))
+    mu = np.matrix(mu)
+    sigma = np.random.random((2,1))
+    sigma = np.matrix(sigma)
+    expec = np.random.random((N,2)) 
+    return X, mu, sigma, alpha, expec, from_1/N    
+       
+def e_step(alpha, mu, sigma, k, N):
+    for i in range(N):
+        denom = 0
+        numer = 0
+        for j in range(k):
+            denom += (alpha[j]/sigma[j])*math.exp(-((X[i]-mu[j])/sigma[j])**2/2)
+        for j in range(k):
+            numer = (alpha[j]/sigma[j])*math.exp(-((X[i]-mu[j])/sigma[j])**2/2)
+            expec[i,j] = numer/denom
+#        print(numer, denom)        
+    return expec
+        
+    
+def m_step(mu, sigma, expec, k, N):
+    for i in range(k):
+        numer_mu = 0
+        numer_sig = 0
+        denom = 0
+        for j in range(N):
+            numer_mu += expec[j,i]*X[j]
+            numer_sig += expec[j,i]*((X[j]-mu[i])**2)
+            denom += expec[j,i]
+        mu[i] = numer_mu/denom
+        sigma[i] = math.sqrt(numer_sig/denom)
+        alpha[i] = denom/N
+    return alpha, mu, sigma
+    
+if __name__ == '__main__':
+    mu = [16,10]
+    sigma = [2,6]
+    epsi = 1e-10
+    k = 2
+    N = 500
+    iter_n = 50
+    true_alpha = [0.3, 0.7]
+    [X, mu, sigma, alpha, expec, ratio] = gen_data(mu, sigma, N)
+    print(ratio)
+    for i in range(iter_n):
+        mu_old = copy.deepcopy(mu)
+        expec = e_step(alpha, mu, sigma, k, N)
+        [alpha, mu, sigma] = m_step(mu, sigma, expec, k, N)
+        print(i, alpha, mu, sigma)
+#        diff = abs(mu - mu_old)
+#        if(diff.mean() < epsi):
+#            break;
+    
+    
