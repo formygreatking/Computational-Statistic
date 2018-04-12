041e6bb876e429c95cb49a10ee6805b9f325558c
CRY <670110891@qq.com>
2018-04-12 21:44:57 +0800

    update

---
+++ experiment4.1/EM-poisson.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def gen_data(lamda_1, lamda_2, N):
+    
+    X = np.zeros((N, 1))
+    X = np.matrix(X)
+    from_1 = 0
+    for i in range(N):
+        if np.random.random() < 0.6:
+            X[i] = stats.poisson.rvs(lamda_1, size=1)
+            from_1 += 1
+        else:
+            X[i] = stats.poisson.rvs(lamda_2, size=1)
+    print(from_1, N-from_1)
+    
+    return X, from_1/N    
+       
+def e_step(alpha, lamb, k, N):
+    for i in range(N):
+        denom = 0
+        numer = 0
+        for j in range(k):
+            denom += alpha[j]*stats.poisson.pmf(X[i], lamb[j])
+        for j in range(k):
+            numer = alpha[j]*stats.poisson.pmf(X[i], lamb[j])
+            expec[i,j] = numer/denom
+#        print(numer, denom)        
+    return expec
+        
+    
+def m_step(lamb, expec, N):
+    numer_p = 0
+    denom = 0
+    numer_q = 0
+    for j in range(N):
+        numer_p += expec[j,0]*X[j]
+        numer_q += expec[j,1]*X[j]
+        denom += expec[j,0]
+    lamb[0] = numer_p/denom
+    lamb[1] = (numer_q)/(N-denom)
+    alpha[0] = denom/N
+    alpha[1] = 1-alpha[0]
+    return alpha, lamb
+    
+if __name__ == '__main__':
+    lamb_true = [14, 5]
+    n = 20
+    epsi = 1e-10
+    k = 2
+    N = 500
+    iter_n = 100
+    #parameter initialion
+    alpha = np.array([[0.5], [0.5]])
+    alpha = np.matrix(alpha)
+    lamb = np.random.random((2,1))
+    lamb = np.matrix(lamb)
+    expec = np.random.random((N,2)) 
+#    true_alpha = [0.3, 0.7]
+    [X, ratio] = gen_data(lamb_true[0], lamb_true[1], N)
+    print(ratio)
+    for i in range(iter_n):
+        lamb_old = copy.deepcopy(lamb)
+        alpha_old = copy.deepcopy(alpha)
+        expec = e_step(alpha, lamb, k, N)
+        [alpha, lamb] = m_step(lamb, expec, N)
+        print(i, alpha, lamb)
+        diff_lamb = abs(lamb - lamb_old)
+        diff_alpha = abs(alpha - alpha_old)
+        if(diff_alpha.mean() < epsi) and (diff_lamb.mean() < epsi):
+            break;
+    
+    
--- experiment4.2/EM-gmm1d.py
+++ experiment4.2/EM-gmm1d.py
@@ -11,20 +11,15 @@ def gen_data(mu, sigma, N):
     from_1 = 0
     from_2 = 0
     for i in range(N):
-        if np.random.random() < 0.6:
+        if np.random.random() < 0.3:
             X[i] = np.random.normal(mu[0], sigma[0], 1)
             from_1 += 1
         else:
             X[i] = np.random.normal(mu[1], sigma[1], 1) 
             from_2 += 1
     print(from_1, from_2)
-    alpha = [0.5, 0.5]
-    mu = np.random.random((2,1))
-    mu = np.matrix(mu)
-    sigma = np.random.random((2,1))
-    sigma = np.matrix(sigma)
-    expec = np.random.random((N,2)) 
-    return X, mu, sigma, alpha, expec, from_1/N    
+
+    return X, from_1/N    
        
 def e_step(alpha, mu, sigma, k, N):
     for i in range(N):
@@ -46,22 +41,30 @@ def m_step(mu, sigma, expec, k, N):
         denom = 0
         for j in range(N):
             numer_mu += expec[j,i]*X[j]
-            numer_sig += expec[j,i]*((X[j]-mu[i])**2)
+#            numer_sig += expec[j,i]*((X[j]-mu[i])**2)
             denom += expec[j,i]
         mu[i] = numer_mu/denom
+        for j in range(N):
+            numer_sig += expec[j,i]*((X[j]-mu[i])**2)
         sigma[i] = math.sqrt(numer_sig/denom)
         alpha[i] = denom/N
     return alpha, mu, sigma
     
 if __name__ == '__main__':
-    mu = [10,1]
-    sigma = [2,2]
+    mu_true = [10,15]
+    sigma = [5,13]
     epsi = 1e-10
     k = 2
     N = 500
-    iter_n = 200
-#    true_alpha = [0.3, 0.7]
-    [X, mu, sigma, alpha, expec, ratio] = gen_data(mu, sigma, N)
+    iter_n = 500
+    #parameter initialion & data generation
+    [X, ratio] = gen_data(mu_true, sigma, N)
+    alpha = [0.5, 0.5]
+    mu = np.array([[X.mean()+1],[X.mean()-1]])
+    mu = np.matrix(mu)
+    sigma = np.array([[X.std()+1],[X.std()-1]])
+    sigma = np.matrix(sigma)
+    expec = np.random.random((N,2)) 
     print(ratio)
     for i in range(iter_n):
         mu_old = copy.deepcopy(mu)
